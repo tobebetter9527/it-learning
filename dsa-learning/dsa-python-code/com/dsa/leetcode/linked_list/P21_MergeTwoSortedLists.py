78a0c55d0a19d588eb123409a957cddf28be1355
tobebetter9527 <wiljz@qq.com>
2023-10-28 21:38:50 +0800

    p21 recursive

--- dsa-learning/dsa-python-code/com/dsa/leetcode/linked_list/P21_MergeTwoSortedLists.py
+++ dsa-learning/dsa-python-code/com/dsa/leetcode/linked_list/P21_MergeTwoSortedLists.py
@@ -21,6 +21,18 @@ class Solution:
         cur.next = list1 if list1 is not None else list2
         return dummy.next
 
+    def mergeTwoLists2(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        if list1.val >= list2.val:
+            list2.next = self.mergeTwoLists2(list1, list2.next)
+            return list2
+        else:
+            list1.next = self.mergeTwoLists2(list1.next, list2)
+            return list1
+
 
 if __name__ == '__main__':
     print("hello world")
