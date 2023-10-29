ec2ee7825656f7679d756d21c30d1b0ee28c6ac8
tobebetter9527 <wiljz@qq.com>
2023-10-29 09:31:56 +0800

    p86

---
+++ dsa-learning/dsa-python-code/com/dsa/leetcode/linked_list/P86_PartitionList.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from com.dsa.leetcode.linked_list import ListNode
+
+
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        newDummy = ListNode()
+        newCur = newDummy
+
+        oldDummy = ListNode()
+        oldCur = oldDummy
+
+        while head:
+            if head.val >= x:
+                oldCur.next = head
+                oldCur = head
+            else:
+                newCur.next = head
+                newCur = head
+            head = head.next
+        oldCur.next = None
+        newCur.next = oldDummy.next
+        return newDummy.next
