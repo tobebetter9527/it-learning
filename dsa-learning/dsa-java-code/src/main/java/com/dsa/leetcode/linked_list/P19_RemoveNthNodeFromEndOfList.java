711d5cd8a4d191cb58e4a2d1657cd939d233528f
tobebetter9527 <wiljz@qq.com>
2023-10-30 18:00:14 +0800

    p19 remove n th node from end of list

---
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/linked_list/P19_RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,20 @@
+package com.dsa.leetcode.linked_list;
+
+public class P19_RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode fast = dummy, slow = dummy;
+
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
