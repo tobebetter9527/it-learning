6c0e07e9c4d03eac94c9c7fadec2bbe06d1323a1
tobebetter9527 <wiljz@qq.com>
2023-10-31 09:10:04 +0800

    p876 middle of the linked list

---
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/linked_list/P876_MiddleOfTheLinkedList.java
@@ -0,0 +1,17 @@
+package com.dsa.leetcode.linked_list;
+
+public class P876_MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
+
