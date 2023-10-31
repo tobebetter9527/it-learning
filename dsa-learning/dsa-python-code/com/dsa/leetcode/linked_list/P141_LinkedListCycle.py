473bc36fcd82a58cc6b814d5eb6678d571e3b4e7
tobebetter9527 <wiljz@qq.com>
2023-10-31 11:30:24 +0800

    p141 linked list cycle

--- dsa-learning/dsa-python-code/com/dsa/leetcode/linked_list/P141_LinkedListCycle.py
+++ dsa-learning/dsa-python-code/com/dsa/leetcode/linked_list/P141_LinkedListCycle.py
@@ -1,2 +1,17 @@
 # author: tobebetter9527
 # since: 2023/10/31 10:37
+from typing import Optional
+
+from com.dsa.leetcode.linked_list import ListNode
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        fast = head.next
+        slow = head
+        while fast and fast.next and fast != slow:
+            fast = fast.next.next
+            slow = slow.next
+        return fast == slow
