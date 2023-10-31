f74ff7e73652855d0f78ed50fcd5089df8b7daf5
tobebetter9527 <wiljz@qq.com>
2023-10-31 10:36:45 +0800

    p141 linked list cycle

---
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/linked_list/P141_LinkedListCycle.java
@@ -0,0 +1,33 @@
+package com.dsa.leetcode.linked_list;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class P141_LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null && fast != slow) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow == fast;
+    }
+
+    public boolean hasCycle2(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (!set.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
