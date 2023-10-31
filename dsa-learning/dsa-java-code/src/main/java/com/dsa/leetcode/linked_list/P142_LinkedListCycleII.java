aae0cf9a859320f9aedcdf01166964d5eb836010
tobebetter9527 <wiljz@qq.com>
2023-10-31 11:30:04 +0800

    p142 linked list cycle II

---
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/linked_list/P142_LinkedListCycleII.java
@@ -0,0 +1,26 @@
+package com.dsa.leetcode.linked_list;
+
+public class P142_LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head, slow = head;
+        while (true) {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
