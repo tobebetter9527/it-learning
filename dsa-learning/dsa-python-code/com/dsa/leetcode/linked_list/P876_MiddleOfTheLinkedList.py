58eceb95645dd095badc03ca7120ad3699634891
tobebetter9527 <wiljz@qq.com>
2023-10-31 10:11:12 +0800

    p876 middle of the linked list

---
+++ dsa-learning/dsa-python-code/com/dsa/leetcode/linked_list/P876_MiddleOfTheLinkedList.py
@@ -0,0 +1,16 @@
+# author: tobebetter9527
+# since: 2023/10/31 9:10
+from typing import Optional
+
+from com.dsa.leetcode.linked_list import ListNode
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        fast, slow = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
