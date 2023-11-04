64fd48b6e49c4bab94ccb6b828eaf23db1011313
tobebetter9527 <wiljz@qq.com>
2023-11-04 20:45:07 +0800

    p167 two sum II input array is sorted

---
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/array/P167_TwoSum2InputArrayIsSorted.java
@@ -0,0 +1,23 @@
+package com.dsa.leetcode.array;
+
+/**
+ * @since 2023/11/4 20:41
+ */
+public class P167_TwoSum2InputArrayIsSorted {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        int temp;
+        while (left < right) {
+            temp = numbers[left] + numbers[right];
+            if (temp == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (temp > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return null;
+    }
+}
