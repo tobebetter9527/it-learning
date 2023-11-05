e04389b6de85f32be9bbb61c08cd12022db0b968
tobebetter9527 <wiljz@qq.com>
2023-11-05 09:49:30 +0800

    p5 longest palindromic substring

---
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/array/P5_LongestPalindromicSubstring.java
@@ -0,0 +1,33 @@
+package com.dsa.leetcode.array;
+
+/**
+ * @since 2023/11/5 08:57
+ */
+public class P5_LongestPalindromicSubstring {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0, length = s.length(); i < length; i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expandAroundCenter(String str, int left, int right) {
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+}
