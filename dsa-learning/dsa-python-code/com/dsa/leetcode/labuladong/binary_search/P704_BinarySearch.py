d7973602b41f32a33b5f14fc0121f71f579243ae
tobebetter9527 <wiljz@qq.com>
2023-11-21 09:03:08 +0800

    p704 binary search

---
+++ dsa-learning/dsa-python-code/com/dsa/leetcode/labuladong/binary_search/P704_BinarySearch.py
@@ -0,0 +1,19 @@
+# author: tobebetter9527
+# since: 2023/11/21 8:57
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
