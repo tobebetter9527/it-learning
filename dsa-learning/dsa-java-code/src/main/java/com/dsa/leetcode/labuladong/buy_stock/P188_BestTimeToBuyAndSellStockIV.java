778395c3197a50c7a686fe5f13d5f2b73b7f7a45
tobebetter9527 <wiljz@qq.com>
2023-11-29 10:51:44 +0800

    p188 best time to buy and sell stock IV

---
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/labuladong/buy_stock/P188_BestTimeToBuyAndSellStockIV.java
@@ -0,0 +1,103 @@
+package com.dsa.leetcode.labuladong.buy_stock;
+
+public class P188_BestTimeToBuyAndSellStockIV {
+
+    public int maxProfit(int k, int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        return recursive(prices, 0, k * 2, 0);
+    }
+
+    /**
+     * 递归
+     *
+     * @param prices  价格
+     * @param idx     第几天
+     * @param doubleK 总买卖次数
+     * @param status  0可以买，1可以卖，2可以买，3可以卖，以此类推
+     * @return max profit
+     */
+    private int recursive(int[] prices, int idx, int doubleK, int status) {
+        if (idx == prices.length) {
+            return 0;
+        }
+        if (status == doubleK) {
+            return 0;
+        }
+        // 可以买
+        if ((status & 1) == 0) {
+            // buy
+            int p1 = -prices[idx] + recursive(prices, idx + 1, doubleK, status + 1);
+            // not buy
+            int p2 = recursive(prices, idx + 1, doubleK, status);
+            return Math.max(p1, p2);
+        } else {
+            // can sell
+            // sell
+            int p1 = prices[idx] + recursive(prices, idx + 1, doubleK, status + 1);
+            // not sell
+            int p2 = recursive(prices, idx + 1, doubleK, status);
+            return Math.max(p1, p2);
+        }
+    }
+
+    private int dp1(int[] prices, int doubleK) {
+        int m = prices.length + 1;
+        int n = doubleK + 1;
+        int[][] dp = new int[m][n];
+        for (int row = 0; row < m; row++) {
+            dp[row][doubleK] = 0;
+        }
+        for (int col = 0; col < n; col++) {
+            dp[m - 1][0] = 0;
+        }
+        // 从下到上，从右到左
+        for (int idx = m - 2; idx >= 0; idx--) {
+            for (int status = n - 2; status >= 0; status--) {
+                if ((status & 1) == 0) {
+                    // buy
+                    int p1 = -prices[idx] + dp[idx + 1][status + 1];
+                    // not buy
+                    int p2 = dp[idx + 1][status];
+                    dp[idx][status] = Math.max(p1, p2);
+                } else {
+                    // can sell
+                    // sell
+                    int p1 = prices[idx] + dp[idx + 1][status + 1];
+                    // not sell
+                    int p2 = dp[idx + 1][status];
+                    dp[idx][status] = Math.max(p1, p2);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    private int dp2(int[] prices, int doubleK) {
+        int m = prices.length + 1;
+        int n = doubleK + 1;
+        int[] dp = new int[n];
+
+        // 从下到上，从右到左
+        for (int idx = m - 2; idx >= 0; idx--) {
+            for (int status = 0; status < doubleK; status++) {
+                if ((status & 1) == 0) {
+                    // buy
+                    int p1 = -prices[idx] + dp[status + 1];
+                    // not buy
+                    int p2 = dp[status];
+                    dp[status] = Math.max(p1, p2);
+                } else {
+                    // can sell
+                    // sell
+                    int p1 = prices[idx] + dp[status + 1];
+                    // not sell
+                    int p2 = dp[status];
+                    dp[status] = Math.max(p1, p2);
+                }
+            }
+        }
+        return dp[0];
+    }
+}
