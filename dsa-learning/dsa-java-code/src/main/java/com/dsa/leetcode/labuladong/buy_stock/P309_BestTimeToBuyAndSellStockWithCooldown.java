793ed335008d4369d9ba35b5e5183f3a7b15ef76
tobebetter9527 <wiljz@qq.com>
2023-11-30 20:33:38 +0800

    P309 best time to buy and sell stock with cooldown

--- dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/labuladong/buy_stock/P309_BestTimeToBuyAndSellStockWithCooldown.java
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/labuladong/buy_stock/P309_BestTimeToBuyAndSellStockWithCooldown.java
@@ -5,7 +5,7 @@ public class P309_BestTimeToBuyAndSellStockWithCooldown {
         if (prices == null || prices.length == 0) {
             return 0;
         }
-        return recursive(prices, 0, 0);
+        return dp(prices);
     }
 
     private int recursive(int[] prices, int i, int status) {
@@ -28,7 +28,34 @@ public class P309_BestTimeToBuyAndSellStockWithCooldown {
         }
     }
 
-    public static void main(String[] args) {
+    private int dp(int[] prices) {
+        int n = prices.length + 2;
+        int m = 2;
+        int[][] dp = new int[n][m];
+        for (int i = n - 3; i >= 0; i--) {
+            for (int status = 0; status < m; status++) {
+                // can buy
+                if (status == 0) {
+                    // buy, you can't buy next day
+                    int p1 = -prices[i] + dp[i + 1][1];
+                    // not buy
+                    int p2 = dp[i + 1][status];
+                    dp[i][status] = Math.max(p1, p2);
+                } else {
+                    // can sell
+                    int p1 = prices[i] + dp[i + 2][0];
+                    // not sell
+                    int p2 = dp[i + 1][status];
+                    dp[i][status] = Math.max(p1, p2);
+                }
+            }
+        }
+        return dp[0][0];
+    }
 
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 0, 2};
+        P309_BestTimeToBuyAndSellStockWithCooldown p309 = new P309_BestTimeToBuyAndSellStockWithCooldown();
+        System.out.println(p309.maxProfit(prices));
     }
 }
