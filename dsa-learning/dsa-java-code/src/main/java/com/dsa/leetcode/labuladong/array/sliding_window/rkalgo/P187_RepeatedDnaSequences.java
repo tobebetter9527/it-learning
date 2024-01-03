d7bb66dfe12b17876b4abdf951802503e6ad5e4e
tobebetter9527 <wiljz@qq.com>
2024-01-03 18:21:48 +0800

    p187

--- dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/labuladong/array/sliding_window/rkalgo/P187_RepeatedDnaSequences.java
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/labuladong/array/sliding_window/rkalgo/P187_RepeatedDnaSequences.java
@@ -1,9 +1,35 @@
 package com.dsa.leetcode.labuladong.array.sliding_window.rkalgo;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class P187_RepeatedDnaSequences {
-    public List<String> findRepeatedDnaSequences(String s) {
-        return null;
+
+
+
+
+    /**
+     * 暴力法，time complexity is O(nl) , l指定字符串长度
+     *
+     * @param s
+     * @return
+     */
+    public List<String> findRepeatedDnaSequences1(String s) {
+        int n = s.length();
+        Set<String> seen = new HashSet<>();
+        Set<String> ans = new HashSet<>();
+        for (int i = 0; i + 10 <= n; i++) {
+            String str = s.substring(i, i + 10);
+            if (seen.contains(str)) {
+                ans.add(str);
+            } else {
+                seen.add(str);
+            }
+
+        }
+        return new ArrayList<>(ans);
     }
+
 }
