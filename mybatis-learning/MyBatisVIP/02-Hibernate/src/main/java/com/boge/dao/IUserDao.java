2236a73b9672e635e3275b5e354e5c178bf327da
tobebetter9527 <30531224+tobebetter9527@users.noreply.github.com>
2025-04-12 15:21:19 +0800

    Delete mybatis-learning directory

--- mybatis-learning/MyBatisVIP/02-Hibernate/src/main/java/com/boge/dao/IUserDao.java
+++
@@ -1,7 +0,0 @@
-package com.boge.dao;
-
-import com.boge.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserDao extends JpaRepository<User,Integer> {
-}
--- mybatis-learning/MyBatisVIP/06-MyBatisSpring/src/main/java/com/boge/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.boge.service;
-
-import com.boge.pojo.User;
-
-import java.util.List;
-
-public interface IUserService {
-    List<User> query();
-}
