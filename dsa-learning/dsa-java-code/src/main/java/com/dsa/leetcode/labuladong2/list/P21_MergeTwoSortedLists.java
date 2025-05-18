984b75e6f86c1373593b1ddaebc0ef5b02c701ff
tobebetter9527 <wiljz@qq.com>
2025-05-18 14:12:56 +0800

    merge two sorted list

---
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/labuladong2/list/P21_MergeTwoSortedLists.java
@@ -0,0 +1,26 @@
+package com.dsa.leetcode.labuladong2.list;
+
+public class P21_MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode p = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                p.next = list2;
+                list2 = list2.next;
+            } else {
+                p.next = list1;
+                list1 = list1.next;
+            }
+            p = p.next;
+        }
+        if (list1 != null) {
+            p.next = list1;
+        }
+        if (list2 != null) {
+            p.next = list2;
+        }
+        return dummy.next;
+    }
+}
