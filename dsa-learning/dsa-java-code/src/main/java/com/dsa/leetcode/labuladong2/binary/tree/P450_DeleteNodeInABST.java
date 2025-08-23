6e9eff94a7dcafb33358a927930ba0527518834d
tobebetter9527 <wiljz@qq.com>
2025-08-23 13:22:51 +0800

    p450

---
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/labuladong2/binary/tree/P450_DeleteNodeInABST.java
@@ -0,0 +1,36 @@
+package com.dsa.leetcode.labuladong2.binary.tree;
+
+public class P450_DeleteNodeInABST {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == key) {
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+
+            TreeNode minNode = getMin(root.right);
+            root.right = deleteNode(root.right, minNode.val);
+            minNode.left = root.left;
+            minNode.right = root.right;
+            root = minNode;
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+
+    private TreeNode getMin(TreeNode cur) {
+        while (cur.left != null) {
+            cur = cur.left;
+        }
+        return cur;
+    }
+}
