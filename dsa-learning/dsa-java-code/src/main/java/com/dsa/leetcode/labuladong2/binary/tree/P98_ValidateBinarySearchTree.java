b61b01f60741f6682587a39bb5d38dca5f081d6b
tobebetter9527 <wiljz@qq.com>
2025-08-23 11:11:03 +0800

    p98

---
+++ dsa-learning/dsa-java-code/src/main/java/com/dsa/leetcode/labuladong2/binary/tree/P98_ValidateBinarySearchTree.java
@@ -0,0 +1,17 @@
+package com.dsa.leetcode.labuladong2.binary.tree;
+
+public class P98_ValidateBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long minValue, long maxValue) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= minValue || root.val >= maxValue) {
+            return false;
+        }
+        return isValidBST(root.left, minValue, root.val) && isValidBST(root.right, root.val, maxValue);
+    }
+}
